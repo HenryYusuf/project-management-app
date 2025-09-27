5bc3bc7621f9998691586883ec10df9967814165
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-09-27 13:21:08 +0700

    changes: eslint rule

--- src/pages/projects/[id].vue
+++ src/pages/projects/[id].vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <h1>Project {{ route.params?.id }}</h1>
+    <h1>Project {{ route.params.id }}</h1>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 
-const route = useRoute()
+const route = useRoute('/projects/[id]')
 </script>
