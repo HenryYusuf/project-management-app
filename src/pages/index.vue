2d3c196b7e06f7ffec139d20b416b74d530d9d1d
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-09-30 15:25:19 +0700

    setup shadcn-vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <h1>Home Page</h1>
+    <Button>Cek Button</Button>
     <RouterLink :to="{ name: '/projects/' }">Go to Projects</RouterLink>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue'
+</script>
